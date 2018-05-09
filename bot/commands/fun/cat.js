9648edc2b7d416dff5f9e698ecf74e18b5a4d5e3
Reforced <reforced@hotmail.nl>
2018-05-09 04:19:04 +0200

    Update cat.js

--- bot/commands/fun/cat.js
+++ bot/commands/fun/cat.js
@@ -3,7 +3,7 @@
 var request = require('request');
 
 module.exports = function(bot) {
-  request('http://random.cat/meow', function (error, response, body) {
+  request('http://aws.random.cat/meow', function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var json = JSON.parse(body);
       bot.sendChat(json.file);
